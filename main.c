3349983978a7c16546aca2220cc1fcb7d7337355
niah-stack <dylanchampion596@hotmail.com>
2025-02-17 18:28:27 +0000

    ASCII, Decimal, Hex, Binary implementation

--- main.c
+++ main.c
@@ -1,56 +1,57 @@
 
 #include <stdio.h>
-void toBinary(char bytes[]);
-void toHex(char bytes[]);
-void toDecimal(char bytes[]);
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+void toBinary(char* input, int size);
+void toHex(char* input, int size);
+void toDecimal(char* input, int size);
+void toASCII(char* input, int size);
 
-    unsigned char bytes[13];
+int main(int argc, char *argv[]){
 
-    bytes[0] = 0b01001101;
-    bytes[1] = 79;
-    bytes[2] = 0b00100000;
-    bytes[3] = 0b01101110;
-    bytes[4] = 61;
-    bytes[5] = 0x6D;
-    bytes[6] = 0b01100101;
-    bytes[7] = 20;
-    bytes[8] = 69;
-    bytes[9] = 73;
-    bytes[10] = 0b00101110;
-    bytes[11] = '.';
-    bytes[12] = 0b00101110;
-    bytes[13] = 0b00100000;
+    if (argc == 1){
+        printf("An argument is needed!");
+    }
 
-    toBinary(bytes);
-    toHex(bytes);
-    toDecimal(bytes);
+    if (argc >= 2){
+        toDecimal(argv[1], strlen(argv[1]));
+        toHex(argv[1], strlen(argv[1]));
+        toBinary(argv[1], strlen(argv[1]));
+        toASCII(argv[1], strlen(argv[1]));
+    }
     
     return 0;
 }
 
-void toBinary(char bytes[]){
+void toBinary(char* input, int size){
     printf("Binary: \n");
-    for (int i = 0; i <= 13; i++){
+    for (int i = 0; i < size; i++){
         for (int b = 7; b >= 0; b--){
-            int bit = (bytes[i] >> b) & 0b00000001;
+            int bit = (input[i] >> b) & 0b00000001;
             printf("%u", bit);
         }
         printf("\n");
     }
 }
 
-void toHex(char bytes[]){
+void toHex(char* input, int size){
     printf("Hex: \n");
-    for (int i = 0; i <= 13; i++){
-        printf("%x \n", bytes[i]);
+    for (int i = 0; i < size; i++){
+        printf("%x \n", input[i]);
     }
 }
 
-void toDecimal(char bytes[]){
+void toDecimal(char* input, int size){
     printf("Decimal: \n");
-    for (int i = 0; i <= 13; i++){
-        printf("%u \n", bytes[i]);
+    for (int i = 0; i < size; i++){
+        printf("%u \n", (int)input[i]);
+    }
+}
+
+void toASCII(char* input, int size){
+    printf("ASCII: \n");
+    for (int i = 0; i < size; i++){
+        printf("%c \n", input[i]);
     }
 }
